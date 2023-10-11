4020ae2461668a96339022846cbe91a35c1ec71d
SittipatSuthin6581126 <146055243+SittipatSuthin6581126@users.noreply.github.com>
2023-10-11 23:05:20 +0700

    Add files via upload
    quadratic equations.

--- main.c
+++ main.c
@@ -3,13 +3,6 @@
 #include <math.h>
 int main()
 {
-    /* double num,sqr_num;
-     printf("enter nmnber  ");
-     scanf("%lf",&num);
-     sqr_num=sqrt(num);
-     printf("Square root of %.2lf is =%.2lf",num,sqr_num);
-*/
-
 
 
 
@@ -19,14 +12,69 @@ int main()
  printf("insert input a,b,c:");
  scanf("%d,%d,%d",&a,&b,&c);
 
+ //ax^2+bx+c;
 
- x1=(-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
- x2=(-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
+ switch (a){
+    case 1: printf("x^2"); break;
+    case 0: break;
+    case -1: printf("-x^2"); break;
+    default: printf("%dx^2",a);
+ }
+if (b!=0 && b>0)printf("+");
+ switch (b){
+    case 0: break;
+    case 1: printf("x"); break;
+    case -1: printf("-x"); break;
+    default: printf("%dx",b);
+    }
+if((a!=0 || b!=0)&&c>0)printf("+");
+ if (c!=0) printf("%d\n",c);
 
- printf("\n x1=%.2f",x1);
-  printf("\n x2=%.2f",x2);
- //(a*pow(x1,2)) + (b*x1) +c;
 
 
-return 0;
-}
+
+    x1=(-b+sqrt(b*b-4*a*c))/(2*a);
+    x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+
+
+
+
+
+  float sq=b*b-4*a*c;
+
+    if (a==0){
+        if(b!=0)
+            printf("\nthe answer = %.2f",-(float)c/b);
+            else puts("no solution");
+    }
+
+    else if(sq==0) printf("One real solution = %.2f",(float)-b/(2*a));
+            else if(sq<0)puts("no real solution");
+                else {
+                    printf("Two real solutions:\n");
+                    printf("x1 = %.2f\n", (-b + sqrt(sq)) / (2 * a));
+                    printf("x2 = %.2f\n", (-b - sqrt(sq)) / (2 * a));
+        }
+
+
+
+
+
+  return 0;
+
+  }
+
+
+
+
+
+ //ax^2+bx+c
+
+
+
+
+
+
+
+
+
